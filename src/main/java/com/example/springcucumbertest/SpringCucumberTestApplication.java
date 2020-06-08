fca1fe2c45475b337b257a9a9b1e9fa481504f9f
Amh <abaams78@gmail.com>
2020-06-07 22:32:49 -0500

    spring web

---
+++ src/main/java/com/example/springcucumbertest/SpringCucumberTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcucumbertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCucumberTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCucumberTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcucumbertest/SpringCucumberTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcucumbertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCucumberTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
